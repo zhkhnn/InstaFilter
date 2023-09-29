5263e770ef40a2bca89fa70377a35129dcf7dfc4
zhkhnn <zhakhanaruzhan@gmail.com>
2023-09-29 16:38:22 +0600

    Initial Commit

---
+++ InstaFilter/InstaFilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaFilterApp.swift
+//  InstaFilter
+//
+//  Created by Aruzhan Zhakhan on 29.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct InstaFilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
